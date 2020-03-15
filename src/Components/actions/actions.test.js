9b9ce2f24cb9dcda870a9d79759311cb369b6cbe
Alexander Makarof <makarofalex@gmail.com>
2020-03-15 20:02:58 +0300

    added some test;

--- src/Components/actions/actions.test.js
+++ src/Components/actions/actions.test.js
@@ -3,9 +3,7 @@ import { deletePost } from "./actions";
 test("deletePost", () => {
   //given
   const result = deletePost();
-
-  //when
   // expect
-
   expect(result.type).toEqual("DELETE_POST");
+  expect(result).toMatchSnapshot();
 });
