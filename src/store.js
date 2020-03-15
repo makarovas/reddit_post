d197b6bcf68acb72067516dea1179a4c343b75e4
Alexander Makarof <makarofalex@gmail.com>
2020-03-15 20:14:33 +0300

    added tracing

--- src/store.js
+++ src/store.js
@@ -7,7 +7,8 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
   )
 );
 store.dispatch(getPosts());
